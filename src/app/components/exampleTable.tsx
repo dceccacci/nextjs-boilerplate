dbc573cd08a38d42e5db1c01c9e3d6bb795c51c4
Tyler Parr <tplaxstar@gmail.com>
2025-05-15 13:35:53 -0700

    table start

--- src/app/components/exampleTable.tsx
+++ src/app/components/exampleTable.tsx
@@ -4,15 +4,14 @@ import { useExampleProvider } from "../context/exampleContext";
 
 
 export default function ExampleTable() {
-  const { pokemon } = useExampleProvider();
+  const { examples } = useExampleProvider();
   const headers = [
     "ID",
     "Name",
-    "Image",
-    "Height",
-    "Weight",
-    "Abilities",
-    "Types"
+    "ImageURL",
+    "arbitrary",
+    "Array",
+    "Object"
   ]
   return (
     <TableContainer>
@@ -26,15 +25,11 @@ export default function ExampleTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {pokemon.map((d: Pokemon, i) =>
+          {examples.map((d: ExampleType, i) =>
             <TableRow key={i}>
               <TableCell>{d.id}</TableCell>
               <TableCell sx={{ textTransform: "capitalize" }}>{d.name}</TableCell>
-              <TableCell><img src={d.imgUrl} style={{ width: 50 }} /></TableCell>
-              <TableCell>{d.height / 10}m</TableCell>
-              <TableCell>{d.weight / 10}kg</TableCell>
-              <TableCell sx={{ textTransform: "capitalize" }}>{d.abilities.join(", ")}</TableCell>
-              <TableCell sx={{ textTransform: "capitalize" }}>{d.types.join(", ")}</TableCell>
+              {/* <TableCell><img src={d.imgUrl} style={{ width: 50 }} /></TableCell> */}
             </TableRow>
           )}
         </TableBody>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { Box } from '@mui/material';
 
 import ExampleSelect from '@/app/components/exampleSelect';
@@ -10,7 +11,7 @@ export default function Home() {
   return (
     <Box>
       <ExampleSelect/>
-      {/* <ExampleTable/> */}
+      <ExampleTable/>
     </Box>
   );
 }
